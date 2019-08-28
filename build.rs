af5fca73c4f16a25a47ba5fd8b3a7b5e12445134
Michael Vlasov <melsomino@gmail.com>
2019-08-28 18:44:03 +0500

    Initial

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ use std::env;
 
 fn main() {
     let root = env!("CARGO_MANIFEST_DIR");
-    println!("cargo:rustc-link-search=native={}/libs", root);
+    println!("cargo:rustc-link-search=native={}", root);
     println!("cargo:rustc-link-lib=dylib=tonclient");
 }
